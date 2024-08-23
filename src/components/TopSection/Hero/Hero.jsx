1e89c765406b8e3f32fd1c331ba7997231927aa0
restlesswizard <erxme@yandex.ru>
2024-08-24 03:01:51 +0500

    feat: hero section initial markup

--- src/components/TopSection/Hero/Hero.jsx
+++ src/components/TopSection/Hero/Hero.jsx
@@ -1,3 +1,32 @@
+import './Hero.css';
+import phone from "./img/samsung.png";
+
 export const Hero = () => {
-    return 'Hero section'
-}
+  return (
+    <div className="wrapper">
+      <section className="hero">
+        <div className="hero-content">
+          <div className="hero-content__col">
+            <h1>
+              Ремонт&nbsp;
+              <span className="brandname">SAMSUNG</span>&nbsp;в Екатеринбурге любой
+              сложности
+            </h1>
+
+            <p className="hero-content__info">
+              Ремонтируем устройства любой сложности в кратчайшие сроки и с
+              гарантией до 3 месяцев
+            </p>
+
+            <button>Заказать ремонт</button>
+          </div>
+          <div className="hero-content__col">
+            <div className="hero-content__image">
+              {/* <img src={phone} alt="Смартфон Samsung" /> */}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
